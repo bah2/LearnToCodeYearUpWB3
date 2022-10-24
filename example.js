3de0120d1f3e6c1e2c31e9e5e973dbdd60ac296e
Ibrahim <ibrahimbahraj@gmail.com>
2022-10-24 13:25:59 -0400

    simple function

---
+++ example.js
@@ -0,0 +1,8 @@
+
+
+function doSomething(){
+    let message = "hello world"
+
+    console.log(message);
+}
+doSomething();
---
+++ simple_functions.js
@@ -0,0 +1,22 @@
+
+function favouriteThings(){
+
+    favouriteThings = "Ibrahim Bah, West_World, West-Life"
+
+    console.log(" what is your name and, What are the three favourite things in your current state?" + "\n" + favouriteThings);
+
+}
+
+function whyImHere(){
+    whyImHere="I joined the Learning community to learn skills in software developement"
+    console.log(whyImHere);
+}
+
+function favouritePlace(){
+    favouritePlace = "New York"
+    console.log("What is the name of your favourite place?" +"\n" +" my favourite place is " + favouritePlace);
+}
+
+favouriteThings();
+whyImHere();
+favouritePlace();
